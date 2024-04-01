082ba400c5f1a87287b5ddd82b78e0015fa78022
Joao Pinheiro <j.pinheiro@jpinhei-ltm942k.internal.salesforce.com>
2024-04-01 09:53:13 -0300

    Sample

--- pipeline/scripts/sh/task-build-deploy.sh
+++ pipeline/scripts/sh/task-build-deploy.sh
@@ -6,22 +6,16 @@ REPOSITORY_PATH="$(git rev-parse --show-toplevel)"
 TEST_ROOT_PATH="$REPOSITORY_PATH/tests"
 DEFAULT_TESTS_PATH="$TEST_ROOT_PATH/default-tests"
 CHECK_ONLY_FLAG=$([[ $CHECK_ONLY == "true" ]] && echo "--checkonly" || echo "")
-#DESTRUCTIVE_CHANGES="./destructiveChanges/destructiveChanges.xml"
 CMD_DEPLOY=("--manifest ./package/package.xml")
 
 cd $REPOSITORY_PATH
 
-# if [ -f "$DESTRUCTIVE_CHANGES" ]; then
-#     CMD_DEPLOY+=("--predestructivechanges "$DESTRUCTIVE_CHANGES"")
-#     echo ""
-#     echo "destructive files found"
-#     echo ""
-# fi
+
 
 echo "\n#### Initiate deploy in Organization #####"
 echo "Running source deploy command with parameters -u "$USERNAME" --testlevel $TEST_LEVEL $CHECK_ONLY_FLAG " 
 ls 
-DEPLOYMENT_INFO="$(sfdx force:source:deploy ${CMD_DEPLOY[@]} -u "$USERNAME"  --testlevel $TEST_LEVEL $CHECK_ONLY_FLAG  --wait 0 --json)"
+DEPLOYMENT_INFO="$(sfdx force:source:deploy -u "$USERNAME"  --testlevel $TEST_LEVEL $CHECK_ONLY_FLAG  --wait 0 --json)"
 echo $DEPLOYMENT_INFO >> ./deployment_info.json
 echo ./deployment_info.json
 DEPLOYMENT_ID="$(jq -r '.result.id' ./deployment_info.json)" 
