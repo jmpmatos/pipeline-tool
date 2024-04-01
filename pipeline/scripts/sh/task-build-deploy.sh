af7768e8696ddc9b43b4aa565ab5c484cef59eea
Joao Pinheiro <j.pinheiro@jpinhei-ltm942k.internal.salesforce.com>
2024-04-01 15:37:53 -0300

    Sample

--- pipeline/scripts/sh/task-build-deploy.sh
+++ pipeline/scripts/sh/task-build-deploy.sh
@@ -15,7 +15,7 @@ cd $REPOSITORY_PATH
 echo "\n#### Initiate deploy in Organization #####"
 echo "Running source deploy command with parameters -u "$USERNAME" --testlevel $TEST_LEVEL $CHECK_ONLY_FLAG " 
 ls
-#sfdx force:source:deploy -u "$USERNAME" --sourcepath ./force-app --testlevel $TEST_LEVEL $CHECK_ONLY_FLAG  --wait 0 --json 
+echo DEFAULT_TESTS_PATH
 DEPLOYMENT_INFO="$(sfdx force:source:deploy -u "$USERNAME" --sourcepath ./force-app --testlevel $TEST_LEVEL $CHECK_ONLY_FLAG  --wait 0 --json)"
 echo $DEPLOYMENT_INFO >> ./deployment_info.json
 echo ./deployment_info.json
